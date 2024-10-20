32359a3ea4d1952ec694d868e7a546b41c1784d6
DILLIP MEHER <DILLIPMEHER@users.noreply.huggingface.co>
2024-10-20 09:41:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 import streamlit as st 
-st.header("This is a Header")
+st.header("JD Parser")
+
+# Take long text input from the user
+user_input = st.text_area("Enter One Job Description:")
+
+# Button to submit the input
+if st.button("Submit"):
+    # Display the user input concatenated with a message
+    st.write(f"Job Description received: {user_input}")
